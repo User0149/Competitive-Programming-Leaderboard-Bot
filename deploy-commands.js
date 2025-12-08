01746388000f463c4c8f948559ce20a52da46cfc
User0149 <floatingprecision@gmail.com>
2025-12-08 22:12:36 +1100

    basic bot functionality with command handlers and deployment

---
+++ deploy-commands.js
@@ -0,0 +1,48 @@
+import { REST, Routes } from "discord.js";
+
+import config from "./config.json" with { type: "json" };
+
+import fs from "fs";
+import path from "path";
+import { fileURLToPath } from "url";
+
+const clientId = config.clientId;
+const token = config.token;
+
+const commands = [];
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const foldersPath = path.join(__dirname, 'commands');
+const commandFolders = fs.readdirSync(foldersPath);
+
+for (const folder of commandFolders) {
+    const commandsPath = path.join(foldersPath, folder);
+    const commandFiles = fs.readdirSync(commandsPath).filter((file) => file.endsWith('.js'));
+    for (const file of commandFiles) {
+        const filePath = path.join(commandsPath, file);
+        const commandModule = await import(`file://${filePath}`);
+        const command = commandModule.default ?? commandModule;
+        // Set a new item in the Collection with the key as the command name and the value as the exported module
+        if ('data' in command && 'execute' in command) {
+            commands.push(command.data.toJSON());
+        } else {
+            console.log(`[WARNING] The command at ${filePath} is missing a required "data" or "execute" property.`);
+        }
+    }
+}
+
+const rest = new REST().setToken(token);
+
+(async () => {
+	try {
+		console.log(`Started refreshing ${commands.length} application (/) commands.`);
+
+		const data = await rest.put(Routes.applicationCommands(clientId), { body: commands });
+
+		console.log(`Successfully reloaded ${data.length} application (/) commands.`);
+	} catch (error) {
+		console.error(error);
+	}
+})();
