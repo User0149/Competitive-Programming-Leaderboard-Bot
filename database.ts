7a970f06e232320bc550ced9f93221fbfc4cb8ad
User0149 <floatingprecision@gmail.com>
2026-01-02 16:54:13 +1100

    created indexes

--- database.ts
+++ database.ts
@@ -2,14 +2,27 @@ import { MongoClient } from "mongodb";
 
 import config from "./config.json" with { type: "json" };
 
+import { Contest, ContestantScores } from "./types/types";
+
 const mongoClient = new MongoClient(config.mongoConnectionString); // don't use stable api as we need all features and don't want to pay
 
 export const leaderboardDb = mongoClient.db("leaderboardDb");
+
+async function ensureIndexes() {
+    const contestsCollection = leaderboardDb.collection<Contest>("contests");
+    const scoresCollection = leaderboardDb.collection<ContestantScores>("scores");
+
+    await contestsCollection.createIndex({ guildID: 1, name: 1 });
+    await scoresCollection.createIndex({ userID: 1, guildID: 1, contestID: 1 });
+}
 
 export async function connectToDatabase() {
     try {
         await mongoClient.connect();
         await mongoClient.db("admin").command({ ping: 1 });
+
+        await ensureIndexes();
+
         console.log("Pinged your deployment. You successfully connected to MongoDB!");
     }
     catch (err) {
