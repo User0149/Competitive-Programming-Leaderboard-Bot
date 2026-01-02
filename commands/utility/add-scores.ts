cce588b6b095ce283e071442137c0a171438bdc8
User0149 <floatingprecision@gmail.com>
2026-01-02 16:10:10 +1100

    added add-scores command

---
+++ commands/utility/add-scores.ts
@@ -0,0 +1,60 @@
+import { ChatInputCommandInteraction, SlashCommandBuilder } from "discord.js";
+import { leaderboardDb } from "../../database.ts";
+
+import type { Contest, ContestantScores } from "../../types/types.ts";
+
+const data = new SlashCommandBuilder()
+	.setName("add-scores")
+	.setDescription("Add your scores for a contest")
+    .addStringOption((option) => option.setName("name").setDescription("The name of the contest").setRequired(true))
+    .addStringOption((option) => option.setName("scores").setDescription("Your scores for each problem, separated by '/' (e.g., 58/100/41)").setRequired(true))
+
+const execute = async (interaction: ChatInputCommandInteraction) => {
+	const guildId = interaction.guildId;
+	const userId = interaction.user.id;
+
+	const interactionOptions = interaction.options;
+	const contestName = interactionOptions.getString("name");
+    const scores = interactionOptions.getString("scores")?.split("/").map(score => Number(score));
+
+	if (!guildId) {
+		throw new Error("Guild ID is null.");
+	}
+	if (!contestName) {
+		throw new Error("Contest name is null.");
+	}
+    if (!scores || scores.some(score => isNaN(score))) {
+        // a score is not a number
+		await interaction.reply(`The format of your scores is invalid. Try again.`);
+        return;
+    }
+
+	const contestsCollection = leaderboardDb.collection<Contest>("contests");
+	const scoresCollection = leaderboardDb.collection<ContestantScores>("scores");
+	const contestDocument = await contestsCollection.findOne({ name: contestName, guildId });
+
+    if (contestDocument === null) {
+		// contest doesn't exist
+		await interaction.reply(`A contest with the the name \`${contestName}\` does not exist.`);
+		return;
+	}
+
+	const contestId = contestDocument._id.toString();
+	const contestProblems = contestDocument.problems;
+
+	if (scores.length !== contestProblems) {
+		await interaction.reply(`Contest \`${contestName}\` has \`${contestProblems}\` problems!`);
+        return;
+	}
+
+	if (await scoresCollection.findOne({ userId, guildId, contestId }) === null) {
+		scoresCollection.insertOne({ userId, guildId, contestId, scores});
+	}
+
+	await interaction.reply(`Added your scores to contest \`${contestName}\`.`);
+};
+
+export default {
+	data,
+	execute
+};
